01df32bd24e604eff365fb71a5615fb43356a025
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-11-18 15:19:57 +0530

    fix: fixed lock merge conflicts

--- src/api/automation/download.ts
+++ src/api/automation/download.ts
@@ -1,16 +1,24 @@
-import { z } from "astro:schema"
-import { api } from "./client"
+import { z } from "astro:schema";
+import { api } from "./client";
 
 export const downloadTypes = z.object({
-    version: z.string(),
-    links: z.object({
-        platform: z.string(),
-        url: z.string().url()
-    }).array()
-})
+  version: z.string(),
+  links: z
+    .object({
+      name: z.string(),
+      os: z.string(),
+      arch: z.string(),
+      extension: z.string(),
+      icon: z.string(),
+      url: z.string().url(),
+    })
+    .array(),
+});
 
 export const getDownloadLinks = async () => {
-    const { data } = await api().get<typeof downloadTypes._type>('/webhook/6a2ad00d-6381-4c79-a801-7768d51a8378')
+  const { data } = await api().get<typeof downloadTypes._type>(
+    "/webhook/6a2ad00d-6381-4c79-a801-7768d51a8378",
+  );
 
-    return downloadTypes.parse(data)
-}
+  return downloadTypes.parse(data);
+};
